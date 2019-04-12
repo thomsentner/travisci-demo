4c84c35c012126f4623e3dcd40f30f7462d494a8
Meeke Roet <Meeke.Roet@klm.com>
2019-04-12 12:15:59 +0200

    Test touppercase endpoint

--- tests/test_flaskapp.py
+++ tests/test_flaskapp.py
@@ -11,12 +11,19 @@ class FlaskTestCase(unittest.TestCase):
     def test_unauthorized(self):
         response = self.app.get('/authorized')
         # Check if the request fails with authorization error
-        self.assertEqual(response._status_code,401,'Unauthorized access to page without login')
+        self.assertEqual(response._status_code, 401, 'Unauthorized access to page without login')
 
     def test_multiply(self):
         response = self.app.get('/multiply?x=5&y=7')
         resp = json.loads(response.data)
-        self.assertEqual(resp['answer'],35,'Multiply endpoint failed known answer 7*5 = 35')
+        self.assertEqual(resp['answer'], 35, 'Multiply endpoint failed known answer 7*5 = 35')
+
+    def test_touppercase(self):
+        expected_output_string = 'HELLOWORLD'
+        actual_output_string = self.app.get('/touppercase?s=helloworld')
+        self.assertEqual(actual_output_string,
+                         expected_output_string,
+                         'Touppercase endpoint failed known answer helloworld > HELLOWORLD')
 
 
     # TODO DEFINE TWO MORE TESTS ON THE END POINTS
