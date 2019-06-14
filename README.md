71b3674abff0f92c7b78fd5b723756d53180eb81
chielP <chiel@xomnia.com>
2019-06-14 11:22:39 +0200

    reset build

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# travisci-demo ![BuildStatus](https://travis-ci.com/MeekeRoet/travisci-demo.png)
+# travisci-demo
 
 Skeleton project of how to use Travis to do CI/CD on a simple flask app
--- tests/test_flaskapp.py
+++ tests/test_flaskapp.py
@@ -11,20 +11,12 @@ class FlaskTestCase(unittest.TestCase):
     def test_unauthorized(self):
         response = self.app.get('/authorized')
         # Check if the request fails with authorization error
-        self.assertEqual(response._status_code, 401, 'Unauthorized access to page without login')
+        self.assertEqual(response._status_code,401,'Unauthorized access to page without login')
 
     def test_multiply(self):
         response = self.app.get('/multiply?x=5&y=7')
         resp = json.loads(response.data)
-        self.assertEqual(resp['answer'], 35, 'Multiply endpoint failed known answer 7*5 = 35')
-
-    def test_touppercase(self):
-        expected_output_string = 'HELLOWORLD'
-        response = self.app.get('/touppercase?s=helloworld')
-
-        self.assertEqual(response.data.decode('utf-8'),
-                         expected_output_string,
-                         'Touppercase endpoint failed known answer helloworld > HELLOWORLD')
+        self.assertEqual(resp['answer'],35,'Multiply endpoint failed known answer 7*5 = 35')
 
 
     # TODO DEFINE TWO MORE TESTS ON THE END POINTS
